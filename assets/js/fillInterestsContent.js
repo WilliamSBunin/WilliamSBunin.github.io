911548ba2cc9b53ca33727dae02bae47724968ec
William Bunin <snowyglaze33@gmail.com>
2023-03-11 19:15:48 -0600

    getting old.

--- assets/js/fillInterestsContent.js
+++ assets/js/fillInterestsContent.js
@@ -15,7 +15,7 @@ navigator.mediaSession.setActionHandler('previoustrack', function() {
     });
     
     window.addEventListener('keydown', function (e) {
-  document.querySelector('body').innerHTML = `You pressed ${e.key}`;
+  document.querySelector('body').innerHTML = "You pressed ${e.key}";
 }, false);
 </script>
 
